7a1bd9d1a852ad5387d0fc227afd05556c68e1d7
MSI\ADMIN <sanglun2254@gmail.com>
2021-05-22 23:52:28 +0700

    test push for protection branch

--- WebAPI/Controllers/BaseApiController.cs
+++ WebAPI/Controllers/BaseApiController.cs
@@ -8,7 +8,8 @@ namespace WebAPI.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     public abstract class BaseApiController : ControllerBase
     {
-        private IMediator _mediator;
+        private IMediator _mediator; 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
+
